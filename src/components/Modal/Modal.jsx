c38b6c27579490965a0655c4de36cf8908501b41
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2022-08-08 14:58:55 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,16 @@
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 import s from './Modal.module.css';
 
 const Modal = ({ objectModal, toggleModal }) => {
-  // useEffect(() => {
-  //   const handleKeyDown = e => {
-  //     if (e.code === 'Escape') {
-  //       toggleModal();
-  //     }
-  //   };
-  //   window.addEventListener('keydown', handleKeyDown);
-  //   return () => window.removeEventListener('keydown', handleKeyDown);
-  // }, []);
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        toggleModal();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
   const handleCloseBackdrop = e => {
     if (e.target.nodeName !== 'DIV') return;
     toggleModal();
